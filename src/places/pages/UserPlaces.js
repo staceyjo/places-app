d349d5580ff05f04275796362367f8d780bccb90
StaceyJo <stacey.l.joseph@gmail.com>
2022-12-28 23:38:21 -0500

    user params succcessfully implemented

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useParams } from "react-router-dom"
 
 import PlaceList from '../components/PlaceList';
 
@@ -30,8 +31,11 @@ const DUMMY_PLACES = [
 ];
 
 const UserPlaces = () => {
+    const userId = useParams().userId;
 
-    return <PlaceList items={DUMMY_PLACES} />;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+
+    return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
